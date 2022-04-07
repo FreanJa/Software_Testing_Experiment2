0cde2280a5665a1aac8c5532fc5e31b9bc5117d9
FreanJa <freanja2730@outlook.com>
2022-04-07 14:25:42 +0800

    Correct the EXP2.java program

--- main/java/EXP2.java
+++ main/java/EXP2.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Objects;
 
 public class EXP2 {
 	public static void main(String[] args) throws IOException {
@@ -23,7 +24,20 @@ public class EXP2 {
 		year = y;
 		month = m;
 		day = d;
-		String result;
+		String result = null;
+
+		if (year < 1900 || year > 2050) {
+			//System.out.println("年的值不在指定范围内");
+			return  "The input year is beyond the valid range!";
+		}
+		else if (month > 12 || month < 1) {
+			//System.out.println("月的值不在指定范围内");
+			return  "The input month is beyond the valid range!";
+		}
+		else if (day > 31 || day < 1) {
+			//System.out.println("日的值不在指定范围内");
+			return  "The input day is beyond the valid range!";
+		}
 
 		switch (month) {
 		case 1:
@@ -32,43 +46,40 @@ public class EXP2 {
 		case 7:
 		case 8:
 		case 10:
-			if (day == 31) { 
+			if (day < 31)
+				day += 1;
+			else {
 				day = 1;
-				month = month + 1;
-			} 
-			else
-				day = day + 1;
+				month += 1;
+			}
 			break;
 		case 4:
 		case 6:
 		case 9:
 		case 11:
-			if (day == 30) {
+			if (day < 30)
+				day += 1;
+			else if (day == 30) {
 				day = 1;
-				month = month + 1;
-			} 
-			else if(day == 31)
-			{
-					System.out.println("Invalid day!");
-					//result = "Invalid day!";
-					//continue;
+				month += 1;
 			}
 			else
-				day = day + 1;
+				result = "Invalid day!";
 			break;
 		case 12:
-			if (day == 31) {
+			if (day < 31)
+				day += 1;
+			else {
 				day = 1;
 				month = 1;
-				year = year + 1;
-			} 
-			else if (day < 31) {
-				day = day + 1;
+				year += 1;
 			}
 			break;
 		case 2: {
-			if (day == 28) {
-				if (((year % 4 == 0 && year % 100 != 0) || year % 400 == 0))
+			if (day < 28)
+				day += 1;
+			else if (day == 28) {
+				if (year % 4 == 0)
 				{
 					day = 29;
 				} 
@@ -77,53 +88,44 @@ public class EXP2 {
 					month = 3;
 				}
 			}
-			else if (day == 29) {
+			else if (day == 29 && year % 4 == 0) {
 				day = 1;
 				month = 3;
-			} 
-			else if (day < 28) {
-				day = day + 1;
-			} 
+			}
 			else{
 				//System.out.println("无效输入日期！");
 				result = "Invalid day!";
-			//continue;
 			}
 		}
 			break;
 		default:
+			result = "Invalid day!";
 		}
+
+		if (Objects.equals(result, "Invalid day!"))
+			return  "The input day is beyond the valid range!";
 		
-		if (year >= 1900 && year <= 2050 && month <= 12 && month >= 1
-				&& day <= 31 && day >= 1) {
+		if (year <= 2050) {
 			//System.out.println("下一天是" + year + "年" + month + "月" + day	+ "日!");
-			StringBuilder months = new  StringBuilder();
+			StringBuilder months = new StringBuilder();
 			StringBuilder days = new StringBuilder();
-			if(month < 10)
-				 months = months.append(0).append(month);
-			else
-				 months =  months.append(month);
-			if(day < 10)
-				days = days.append(0).append(day);
-			else
-				days = days.append(day);
-			StringBuilder resultB = new StringBuilder().append(year).append(months).append(days);
-			result = resultB.toString();
+			if(month < 10) {
+				months.append(0).append(month);
+			} else {
+				months.append(month);
+			}
+			if(day < 10) {
+				days.append(0).append(day);
+			} else {
+				days.append(day);
+			}
+			result = String.valueOf(year) + months + days;
 		} 
-		else if (year < 1900 || year > 2050) {
+		else {
 			//System.out.println("年的值不在指定范围内");
-			result = "The input year is beyond the valid range!"; 
-		} 
-		else if (month > 12 || month < 1) {
-			//System.out.println("月的值不在指定范围内");
-			result = "The input month is beyond the valid range!"; 
-		} 
-		else if (day > 31 || day < 1) {
-			//System.out.println("日的值不在指定范围内");
-			result = "The input day is beyond the valid range!"; 
+			result = "The input year is beyond the valid range!";
 		}
-		else 
-			result = null;	
+
 		return result;
 	}
 }
